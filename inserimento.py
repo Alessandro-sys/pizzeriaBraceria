d15f49ca4ea30b40fc425c9bbdf81626aeced32d
Alessandro <71657415+Alessandro-sys@users.noreply.github.com>
2023-08-20 17:30:03 +0200

    Modifiche secondarie

--- inserimento.py
+++ inserimento.py
@@ -1,12 +1,23 @@
+# from cs50 import SQL
+
+# db = SQL("sqlite:///database.db")
+# while True:
+#     nome = input("Nome: ")
+#     prezzo = input("Prezzo: ")
+#     descrizione = input("Descrizione: ")
+
+#     if not descrizione:
+#         db.execute("INSERT INTO antipasti (food_name, price) VALUES (?, ?)", nome, prezzo)
+#     else:
+#         db.execute("INSERT INTO antipasti (food_name, price, description) VALUES (?, ?, ?)", nome, prezzo, descrizione)
+
+
 from cs50 import SQL
 
 db = SQL("sqlite:///database.db")
-while True:
-    nome = input("Nome: ")
-    prezzo = input("Prezzo: ")
-    descrizione = input("Descrizione: ")
 
-    if not descrizione:
-        db.execute("INSERT INTO antipasti (food_name, price) VALUES (?, ?)", nome, prezzo)
-    else:
-        db.execute("INSERT INTO antipasti (food_name, price, description) VALUES (?, ?, ?)", nome, prezzo, descrizione)
+antipasti = db.execute("SELECT * FROM antipasti")
+
+for element in antipasti:
+    if element["description"] == None:
+        db.execute("UPDATE antipasti SET description = '' WHERE food_name = ?", element["food_name"])
